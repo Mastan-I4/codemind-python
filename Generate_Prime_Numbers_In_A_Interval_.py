1f4e60a54eeb663b11e73788dcaea63d7b557f5d
Mastan-I4 <84570470+Mastan-I4@users.noreply.github.com>
2022-11-03 10:39:16 +0530

    n1=int(input()) n2=int(input()) def ispr(n):     if n==1:         return False     for i in range(2,int(n**0.5)+1):         if n%i==0:             return False     return True
    for i in range(n1,n2):
        if ispr(i):
            print(i)

---
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -0,0 +1,13 @@
+n1=int(input())
+n2=int(input())
+def ispr(n):
+    if n==1:
+        return False
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
+    return True
+
+for i in range(n1,n2):
+    if ispr(i):
+        print(i)
